e192a6bcbc7464a91e42abb18ab11058499d8b0c
tyang <mlscience@hotmail.com>
2025-09-15 07:20:35 -0700

    更新库增加未接通不编码传送音频

--- main/libs/sip/mips/sdk/include/sip_sdk_media.h
+++ main/libs/sip/mips/sdk/include/sip_sdk_media.h
@@ -106,23 +106,24 @@ extern "C"
     } sip_sdk_audio_codec;
     typedef struct sip_sdk_media_config
     {
+        sdk_bool_t audio_only_call_confirmed;                       // 只有呼叫确认后在编解码音频（默认SDK_FALSE）
         sip_sdk_audio_codec audio_codec[SDK_MAX_AUDIO_CODEC_COUNT]; // 音频编码配置
-        int audio_clock_rate;               // 音频时钟速率（默认 16000)
-        float mic_gain;                     // mic增益（默认 1)
-        float speaker_gain;                 // speaker增益（默认 1)
-        sdk_bool_t ns_enable;               // 是否启用噪声抑制（默认 SDK_TRUE）
-        int ns_strength;                    // 噪声抑制强度(默认 -20，-1～-40)
-        sdk_bool_t agc_enable;              // 是否启用自动增益（默认 SDK_TRUE）
-        sdk_bool_t aec_enable;              // 是否启用回音消除（默认 SDK_TRUE）
-        short int aec_elimination_time;     // 回音消除时间 （默认 30 ms）
-        sip_video_op video_op;              // 视频操作
-        sip_audio_op audio_op;              // 音频操作
-        sdk_bool_t not_enable_encode;       // 不启用编码（关闭可以省下一些内存）（默认 SDK_FALSE)
-        sdk_bool_t not_enable_decode;       // 不启用解码（关闭可以省下一些内存）（默认 SDK_FALSE)
-        unsigned decode_max_width;          // 解码支持最大宽度（默认 640)
-        unsigned decode_max_height;         // 解码支持最大高度 (默认 480)
-        sdk_bool_t combin_sps_pps_idr;      // 组合sps pps idr (默认 SDK_FALSE)
-        sip_sdk_media_h264_fmtp h264_fmtp;  // H264 fmtp
+        int audio_clock_rate;                                       // 音频时钟速率（默认 16000)
+        float mic_gain;                                             // mic增益（默认 1)
+        float speaker_gain;                                         // speaker增益（默认 1)
+        sdk_bool_t ns_enable;                                       // 是否启用噪声抑制（默认 SDK_TRUE）
+        int ns_strength;                                            // 噪声抑制强度(默认 -20，-1～-40)
+        sdk_bool_t agc_enable;                                      // 是否启用自动增益（默认 SDK_TRUE）
+        sdk_bool_t aec_enable;                                      // 是否启用回音消除（默认 SDK_TRUE）
+        short int aec_elimination_time;                             // 回音消除时间 （默认 30 ms）
+        sip_video_op video_op;                                      // 视频操作
+        sip_audio_op audio_op;                                      // 音频操作
+        sdk_bool_t not_enable_encode;                               // 不启用编码（关闭可以省下一些内存）（默认 SDK_FALSE)
+        sdk_bool_t not_enable_decode;                               // 不启用解码（关闭可以省下一些内存）（默认 SDK_FALSE)
+        unsigned decode_max_width;                                  // 解码支持最大宽度（默认 640)
+        unsigned decode_max_height;                                 // 解码支持最大高度 (默认 480)
+        sdk_bool_t combin_sps_pps_idr;                              // 组合sps pps idr (默认 SDK_FALSE)
+        sip_sdk_media_h264_fmtp h264_fmtp;                          // H264 fmtp
     } sip_sdk_media_config;
 
     typedef struct sip_sdk_media_connect_param
